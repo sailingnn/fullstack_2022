a795a67db195b21dbd481473ab2beca1eb13ab60
sailingnn <457886446@qq.com>
2022-04-12 13:32:56 +0800

    part1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,9 +11,7 @@ const Content = (props) => {
   // console.log(props)
   return (
     <div>
-      <p>
-       {props.text} {props.number}
-      </p>
+      <p> {props.text} {props.number} </p>
     </div>
   )
 }
@@ -22,22 +20,87 @@ const Button = (props) => (
     {props.text}
   </button>
 )
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <Content text="good" number={props.good} />
+      <Content text="neutral" number={props.neutral} />
+      <Content text="bad" number={props.bad} />
+      <Content text="all" number={props.sum} />
+      <Content text="average" number={props.average} />
+      <Content text="positive" number={props.positive} />
+    </div>
+  )
+}
+
+const Average = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat(1))
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat(0))
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setAll(allClicks.concat(-1))
+    setBad(bad + 1)
+  }
+  const average = function(array) {
+    let len = array.length
+    let sum = 0
+    if(len === 0){
+      return 0
+    }
+    for(let i=0;i<len;i++){
+      sum += array[i]
+    }
+    return sum/len
+  }
+
+  const positive = function(array) {
+    let len = array.length
+    let sum = 0
+    if(len === 0){
+      return 0
+    }
+    for(let i=0;i<len;i++){
+      if(array[i] > 0){
+        sum += 1
+      }
+    }
+    return sum * 100 /len
+  }
 
   return (
     <div>
       <Header name={"give feedback"} />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <Header name={"statistics"} />
-      <Content text="good" number={good} />
-      <Content text="neutral" number={neutral} />
-      <Content text="bad" number={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} sum={good + neutral + bad} average={average(allClicks)} positive={positive(allClicks) + ' %'} />
     </div>
   )
 }
